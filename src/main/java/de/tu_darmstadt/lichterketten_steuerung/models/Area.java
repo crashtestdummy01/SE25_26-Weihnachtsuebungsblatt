586fae8775f4f4d886099aa525db28ced2b1a138
crashtestdummy01 <testdummyt01@gmail.com>
2025-12-04 19:49:21 +0100

    feat(model): add areas

---
+++ src/main/java/de/tu_darmstadt/lichterketten_steuerung/models/Area.java
@@ -0,0 +1,10 @@
+package de.tu_darmstadt.lichterketten_steuerung.models;
+
+import java.util.List;
+
+public record Area(int id, List<StringLight> stringLights, String name) {
+    public Area {
+        if (name == null || name.isEmpty())
+            name = "Area " + id;
+    }
+}
