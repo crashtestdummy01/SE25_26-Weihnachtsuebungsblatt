28dd7b9753020d51c7fbb8575590009f96f5c63f
crashtestdummy01 <testdummyt01@gmail.com>
2025-12-04 23:09:57 +0100

    feat(app): add nimbus look and feel

--- src/main/java/de/tu_darmstadt/lichterketten_steuerung/App.java
+++ src/main/java/de/tu_darmstadt/lichterketten_steuerung/App.java
@@ -1,28 +1,19 @@
 package de.tu_darmstadt.lichterketten_steuerung;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import de.tu_darmstadt.lichterketten_steuerung.view.MainWindow;
+
+import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
-        // Swing components should be created on the Event Dispatch Thread
-        SwingUtilities.invokeLater(App::createAndShowGUI);
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception ignored) {}
+
+        SwingUtilities.invokeLater(App::showGUI);
     }
 
-    private static void createAndShowGUI() {
-        // Create the window (JFrame)
-        JFrame frame = new JFrame("Simple Maven App");
-
-        // Set the operation to close the application when the X is clicked
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Set the size of the window (width, height)
-        frame.setSize(400, 300);
-
-        // Center the window on the screen
-        frame.setLocationRelativeTo(null);
-
-        // Display the window
-        frame.setVisible(true);
+    private static void showGUI() {
+        new MainWindow();
     }
 }
